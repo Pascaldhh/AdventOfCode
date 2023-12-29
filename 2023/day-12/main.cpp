e65b70ab8f1c85b43b6a0705ccc423286d99a5c5
Pascaldhh <haan.pascal@hotmail.com>
2023-12-29 13:30:32 +0100

    Day-12 part 2 for 2023 completed

--- 2023/day-12/main.cpp
+++ 2023/day-12/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
+#include <map>
+enum class Spring {
+    Operational,
+    Damaged,
+    Unknown
+};
 
 struct Record {
-    std::vector<char> springs;
+    std::vector<Spring> springs;
     std::vector<int> numbers;
-    std::vector<std::vector<char>> splitSprings();
-    std::vector<int> amountSolveEachSplit();
+    static std::map<std::tuple<std::vector<Spring>, std::vector<int>>, long> cache;
+    long countVariations(std::vector<Spring>, std::vector<int>);
+    void unfold(int n);
     std::string str();
 };
 struct Records { std::vector<Record> list; };
@@ -17,30 +25,38 @@ void partTwo(Records);
 int main() {
     std::ifstream input("../input.txt");
     if (!input.is_open()) return -1;
-    
+
     Records records = getRecords(input);
+    
     partOne(records);
     partTwo(records);
-    
+
     input.close();
     return 0;
 }
 
 void partOne(Records records) {
-    for (Record record : records.list) {
-        for (int n : record.amountSolveEachSplit()) std::cout << n;
-        std::cout << std::endl;
-        std::cout << record.str() << std::endl;
-    }
+    long sum = 0;
+    for (Record record : records.list)
+        sum += record.countVariations(record.springs, record.numbers);
+    
+    std::cout << "Answer part 1: " << sum << std::endl;
 }
 void partTwo(Records records) {
+    long sum = 0;
+    int i = 0;
+    for (Record record : records.list) {
+        record.unfold(4);
+        sum += record.countVariations(record.springs, record.numbers);
+    }
 
+    std::cout << "Answer part 2: " << sum << std::endl;
 }
 
 std::string Record::str() {
     std::string string = "Record[springs: [";
     for (int i = 0; i < springs.size(); i++) {
-        string += springs[i];
+        string += std::to_string(static_cast<int>(springs[i]));
         if (i != springs.size()-1) string += ',';
     }
     string += "], numbers: [";
@@ -48,99 +64,69 @@ std::string Record::str() {
         string += std::to_string(numbers[i]);
         if (i != numbers.size()-1) string += ',';
     }
-    string += "]]";
-    return string;
+    return string + "], variations: " + std::to_string(countVariations(springs, numbers)) + "]";
 }
 
-std::vector<std::vector<char>> Record::splitSprings() {
-    std::vector<std::vector<char>> allSprings;
+void Record::unfold(int n) {
+    std::vector<Spring> sps = springs;
+    std::vector<int> nums = numbers;
     
-    std::string string;
-    for(int i = 0; i < springs.size(); i++) {
-        char c = springs[i];
-        if (c == '?' || c == '#') string += c;
-        if (c == '.') {
-            if (string.empty()) continue;
-            
-            allSprings.emplace_back(string.begin(), string.end());
-            string = "";
+    for (int i = 0; i < n; i++) {
+        springs.push_back(Spring::Unknown);
+        springs.insert(springs.end(), sps.begin(), sps.end());
+        numbers.insert(numbers.end(), nums.begin(), nums.end());
+    }
+}
+
+std::map<std::tuple<std::vector<Spring>, std::vector<int>>, long> Record::cache;
+long Record::countVariations(std::vector<Spring> sprngs, std::vector<int> nums) {
+    if (sprngs.empty()) return nums.empty() ? 1 : 0;
+    if (nums.empty()) return std::find(sprngs.begin(), sprngs.end(), Spring::Damaged) != sprngs.end() ? 0 : 1;
+
+    std::tuple<std::vector<Spring>, std::vector<int>> key = std::make_tuple(sprngs, nums);
+    if (Record::cache.find(key) != Record::cache.end())
+        return Record::cache[key];
+
+    long count = 0;
+    if (sprngs[0] == Spring::Unknown || sprngs[0] == Spring::Operational)
+        count += countVariations(std::vector<Spring>(sprngs.begin()+1, sprngs.end()), nums);
+
+    if (sprngs[0] == Spring::Unknown || sprngs[0] == Spring::Damaged) {
+        std::vector<Spring> tempVec(sprngs.begin(), sprngs.begin()+nums[0]);
+        if (nums[0] <= sprngs.size() && std::find(tempVec.begin(), tempVec.end(), Spring::Operational) == tempVec.end() &&
+            (nums[0] == sprngs.size() || sprngs[nums[0]] != Spring::Damaged)) {
+            if (sprngs.begin()+nums[0]+1 > sprngs.end()) count += countVariations({}, std::vector<int>(nums.begin()+1, nums.end()));
+            else count += countVariations(std::vector<Spring>(sprngs.begin()+nums[0]+1, sprngs.end()), std::vector<int>(nums.begin()+1, nums.end()));
         }
-        if (i == springs.size()-1 && !string.empty()) 
-            allSprings.emplace_back(string.begin(), string.end());
     }
-    
-    return allSprings;
-}
-
-int countSinglePositions(int n, std::vector<char> v) {
-    int count = 0;
-    for (int i = 0; i < v.size(); i++) if (i+n <= v.size()) count++;
+    Record::cache[key] = count;
     return count;
-}
-
-int countMultiplePositions(std::vector<int> nums, std::vector<char> vec) {
-    int solve = 0;
-    
-    return solve;
-}
-
-//void countAndEraseSingleOccurrence(std::vector<int> &amount, std::vector<std::vector<char>> &sp, std::vector<int> &nums) {
-//    for (int i = nums.size()-1; i >= 0; i--) {
-//        int count = 0, index = 0;
-//        for (int j = 0; nums[i] <= sp[j].size(); j++) {
-//            count++;
-//            index = j;
-//        }
-//        if (count != 1) continue;
-//        std::cout << count << std::endl;
-//        amount[i] += countSinglePositions(nums[i], sp[index]);
-//        nums.erase(nums.begin() + i);
-//        sp.erase(sp.begin() + index);
-//    }
-//}
-
-
-std::vector<int> Record::amountSolveEachSplit() {
-    std::vector<std::vector<char>> sp = this->splitSprings();
-    std::vector<int> amount(this->numbers.size(), 0);
-    std::vector<int> nums = this->numbers;
-
-    //Same size nums and springs
-    if (nums.size() == sp.size()) {
-        for (int i = 0; sp.size() > i; i++)
-            amount[i] = countSinglePositions(nums[i], sp[i]);
-        return amount;
-    }
-    
-    
-    std::cout << "NUMS: ";
-    for (int n : nums) std::cout << n << " ";
-    std::cout << std::endl;
-    
-    for (std::vector<char> v : sp) {
-        for (char c : v) std::cout << c;
-        std::cout << std::endl;
-    }
-    
-    return amount;
 }
 
 Records getRecords(std::ifstream &input) {
     Records records;
-    
+
     std::string line;
     while(std::getline(input, line)) {
         Record record;
         std::string split;
         std::stringstream ss(line);
         for(int i = 0; std::getline(ss, split, ' '); i++) {
-            if (i == 0) record.springs = std::vector<char>(split.begin(), split.end());
+            if (i == 0) {
+                std::vector<Spring> springs;
+                for (char c : split) {
+                    if (c == '?') springs.push_back(Spring::Unknown);
+                    if (c == '#') springs.push_back(Spring::Damaged);
+                    if (c == '.') springs.push_back(Spring::Operational);
+                }
+                record.springs = springs;
+            }
             if (i == 1) {
-                std::string number;
+                std::string string;
                 std::stringstream ss2(split);
                 std::vector<int> numbers;
-                while(std::getline(ss2, number, ',')) {
-                    numbers.push_back(std::stoi(number));
+                while(std::getline(ss2, string, ',')) {
+                    numbers.push_back(std::stoi(string));
                 }
                 record.numbers = numbers;
             }
