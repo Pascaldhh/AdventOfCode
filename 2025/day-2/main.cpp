d3fb79dbea39faba97cbc7bfdad5f8b3e60022bb
Pascaldhh <haan.pascal@hotmail.com>
2026-01-08 11:29:16 +0100

    Day-2 part 1 & part 2 2025 completed

--- 2025/day-2/main.cpp
+++ 2025/day-2/main.cpp
@@ -5,14 +5,15 @@
 #include <vector>
 
 struct Range {
-    long from, to;
+    long long from, to;
 
-    std::vector<int> get_invalid_ids();
+    std::vector<long long> get_invalid_ids();
+    std::vector<long long> get_invalid_ids_two();
     std::string str();
     static std::vector<Range> get_all(std::ifstream &input);
 };
 
-bool is_even(const int &input);
+bool is_even(const long long &input);
 void part_one(std::vector<Range> &ranges);
 void part_two(std::vector<Range> &ranges);
 
@@ -32,9 +33,9 @@ int main() {
 }
 
 void part_one(std::vector<Range> &ranges) {
-    int sum = 0;
+    long long sum = 0;
     for (Range &range : ranges) {
-        for (int id : range.get_invalid_ids()) {
+        for (long long id : range.get_invalid_ids()) {
             sum += id;
         }
     }
@@ -42,11 +43,17 @@ void part_one(std::vector<Range> &ranges) {
 }
 
 void part_two(std::vector<Range> &ranges) {
-    std::cout << "Answer part two: " << std::endl;
+    long long sum = 0;
+    for (Range &range : ranges) {
+        for (long long id : range.get_invalid_ids_two()) {
+            sum += id;
+        }
+    }
+    std::cout << "Answer part two: " << sum << std::endl;
 }
 
-std::vector<int> Range::get_invalid_ids() {
-    std::vector<int> result;
+std::vector<long long> Range::get_invalid_ids() {
+    std::vector<long long> result;
 
     std::string from_str = std::to_string(from);
     std::string to_str = std::to_string(to);
@@ -54,7 +61,7 @@ std::vector<int> Range::get_invalid_ids() {
     if (!is_even(from_str.size()) &&
         from_str.length() == to_str.length()) return result;
 
-    for (int num = from; num <= to; num++) {
+    for (long long num = from; num <= to; num++) {
         std::string num_str = std::to_string(num);
         int num_length = num_str.length();
 
@@ -67,6 +74,35 @@ std::vector<int> Range::get_invalid_ids() {
 
     return result;
 }
+
+std::vector<long long> Range::get_invalid_ids_two() {
+    std::vector<long long> result;
+
+    std::string from_str = std::to_string(from);
+    for (long long num = from; num <= to; num++) {
+        std::string num_str = std::to_string(num);
+
+        for (int i = 1; i <= num_str.length() / 2; i++) {
+            std::string first_part_num = num_str.substr(0, i);
+
+            bool done = false;
+
+            for (int j = i; j < num_str.length(); j += i) {
+                std::string second_sub = num_str.substr(j, i);
+                if (first_part_num != second_sub) break;
+                if (j+i >= num_str.length()) done = true;
+            }
+
+            if (done) {
+                result.push_back(num);
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
 
 std::vector<Range> Range::get_all(std::ifstream &input) {
     std::vector<Range> result;
@@ -90,7 +126,7 @@ std::string Range::str() {
     return std::format("Range[from={},to={}]", from, to);
 }
 
-bool is_even(const int &input) {
+bool is_even(const long long &input) {
     return input % 2 == 0;
 }
 
