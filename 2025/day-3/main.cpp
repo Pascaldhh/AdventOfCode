f0c51571b3e7864689f1c48017f0488d43bdf826
Pascaldhh <haan.pascal@hotmail.com>
2026-01-09 16:23:30 +0100

    Day-3 part 1 & 2 done 2025

--- 2025/day-3/main.cpp
+++ 2025/day-3/main.cpp
@@ -1,16 +1,17 @@
+#include <format>
 #include <iostream>
 #include <fstream>
 
 struct BatteryBank {
     std::string value;
 
-    int get_largest_joltage();
+    long long get_largest_joltage(int length = 2) const;
 
     static int get_all(std::ifstream &input, BatteryBank **banks);
 };
 
-void part_one();
-void part_two();
+void part_one(int size, BatteryBank* banks);
+void part_two(int size, BatteryBank* banks);
 
 int main() {
     std::ifstream input("../input.txt");
@@ -19,8 +20,8 @@ int main() {
     BatteryBank* banks = nullptr;
     int size = BatteryBank::get_all(input, &banks);
 
-    part_one();
-    part_two();
+    part_one(size, banks);
+    part_two(size, banks);
 
     input.close();
 
@@ -29,20 +30,45 @@ int main() {
     return 0;
 }
 
-void part_one() {
-
+void part_one(int size, BatteryBank* banks) {
+    long long sum = 0;
+    for (int i = 0; i < size; i++) {
+        long long value = banks[i].get_largest_joltage();
+        sum += value;
+    }
+    std::cout << "Answer part one: " << sum << std::endl;
 }
 
-void part_two() {
-
+void part_two(int size, BatteryBank* banks) {
+    long long sum = 0;
+    for (int i = 0; i < size; i++) {
+        long long value = banks[i].get_largest_joltage(12);
+        sum += value;
+    }
+    std::cout << "Answer part two: " << sum << std::endl;
 }
 
-int BatteryBank::get_largest_joltage() {
-    for() {
-        BatteryBank inner(this->value.substr(1));
+long long BatteryBank::get_largest_joltage(int length) const {
+    std::string result = "";
+
+    for (int i = 0; i < value.size(); i++) {
+        char current_cn = value[i];
+        bool been_inserted = false;
+
+        int offset = i - value.size() + length;
+        for (int j = result.size()-1; j >= (offset < 0 ? 0 : offset); j--) {
+            char current_r_cn = result[j];
+            if (current_r_cn >= current_cn) continue;
+
+            been_inserted = true;
+            result.erase(j);
+            result.insert(j, { current_cn });
+        }
+
+        if (!been_inserted && result.size() < length) result.append({ current_cn });
     }
 
-
+    return std::stoll(result);
 }
 
 int BatteryBank::get_all(std::ifstream &input, BatteryBank **banks) {
